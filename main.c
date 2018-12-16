6e3bb848559365da44b47a079a7ef4f0a6997a31
vecnehladny <viktorklima@yahoo.com>
2018-12-16 16:16:53 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Refactoring
+//
+//  Created by Viktor Klíma on 16/12/18.
+//  Copyright © 2018 Viktor Klíma. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main()
+{
+  
+    
+    return 0;
+}
