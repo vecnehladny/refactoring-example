03cf5a10671a1f9f220e5e7be995e3c96c2147e4
vecnehladny <viktorklima@yahoo.com>
2018-12-16 16:23:53 +0100

    Pridane citanie

--- main.c
+++ main.c
@@ -19,7 +19,13 @@ int main()
 {
     int n;
     
+    scanf("%d", &n);
     
+    BOD v[n];
+    
+    for(int i = 0;i<n;i++){
+        scanf("%d %d",v[i].x,&v[i].y);
+    }
     
     return 0;
 }
